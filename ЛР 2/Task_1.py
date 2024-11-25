0ee40ce3ef1a4771020abeccaa374371b80f49de
egor-mart <egormurt@gmail.com>
2024-11-25 23:02:21 +0300

    Update Task_1.py

--- ЛР 2/Task_1.py
+++ ЛР 2/Task_1.py
@@ -2,10 +2,11 @@ money_capital = 20000  # Подушка безопасности
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 increase = 0.05  # Ежемесячный рост цен
-
 month = 0
-while money_capital + salary > spend:
+
+max_months = 12  
+    while month < max_months and (money_capital + salary) > spend:
     month += 1
     money_capital = money_capital + salary - spend
-    spend = spend * 1.05
+    spend = spend + (spend * increase)
 print("Количество месяцев, которое можно протянуть без долгов:", month)
