c147cad14ae650e880288c4448c82bd59332dadf
egor-mart <egormurt@gmail.com>
2024-11-28 22:51:15 +0300

    Update Task_2.py

--- ЛР 2/Task_2.py
+++ ЛР 2/Task_2.py
@@ -3,7 +3,7 @@ spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 money_capital = 0
-for _ in range(10):
+for _ in range(month):
     money_capital = money_capital + spend - salary
     spend = spend + (spend * increase)
 
